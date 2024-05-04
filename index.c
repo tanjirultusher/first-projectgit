695f2dab6cb27c640903eadd788838fdcf87d70c
tanjirultusher <tanjirultusher2030@gmail.com>
2024-05-04 21:15:59 +0600

    Create index.c

---
+++ index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    printf("this is my first github repository creation\n");
+    printf("and file uploaded");
+
+    return 0;
+    
+}
